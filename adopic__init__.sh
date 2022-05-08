f0fb8bf894498d332b97327bd8879317c24b038a
Adonay Pichardo <adonaypichardo@gmail.com>
2022-05-08 15:35:03 -0700

    finished bash script to automate downloading preferred tools

--- adopic__init__.sh
+++ adopic__init__.sh
@@ -1,13 +1,40 @@
 #! /usr/bin/bash
 
-allTools="pip3 python3 gcc g++ nasm git"
+allTools="python3-pip python3 gcc g++ make nasm git sublime3"
+
+sudo apt update
+sudo apt upgrade
 
 for targetBinary in $allTools;
 do
-    echo "[+] Installing: " $targetBinary
-    case targetBinary in
-        "python3-pip")
-            apt-get install python3-pip
-            ;;
-    esac
+	echo "[+] Installing:" $targetBinary
+	case $targetBinary in
+	"python3-pip")
+		sudo apt-get install python3-pip
+	;;
+	"python3")
+		sudo apt-get install python3
+	;;
+	"gcc" | "g++" | "make")
+		sudo apt install build-essential
+		sudo apt-get install manpages-dev
+	;;
+	"nasm")
+		sudo apt install nasm
+	;;
+	"git")
+		sudo apt install git-all
+	;;
+	"sublime3")
+		wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+		sudo apt-get install apt-transport-https
+		echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+		sudo apt-get update
+		sudo apt-get install sublime-text
+	;;
+	*)
+		echo "[!] Unrecognized tool:_>" $targetBinary
+		echo "[.] Skipping over instead."
+	;;
+	esac
 done
